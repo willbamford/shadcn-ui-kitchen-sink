3a741d5b74a6725d5d1e03b8778751f760a3f7eb
Will Bamford <will.bamford@twinkl.co.uk>
2024-06-30 15:24:07 +0100

    Add accordion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,41 @@
 import { Button } from "@/components/ui/button";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 export default function Home() {
   return (
     <main className="p-8">
       <h1 className="text-4xl font-bold mb-8">shadcn/ui kitchen sink</h1>
+
       <section className="mb-8">
-        <h2 className="text-lg font-medium mb-4">Buttons</h2>
-        <div className="flex gap-4 flex-wrap">
-          <Button variant="default">Default</Button>
-          <Button variant="secondary">Secondary</Button>
-          <Button variant="destructive">Destructive</Button>
-          <Button variant="ghost">Ghost</Button>
-          <Button variant="link">Link</Button>
-          <Button variant="outline">Outline</Button>
-          <Button className="w-full">Full Width</Button>
-          <Button size="icon">Ic.</Button>
-          <Button size="sm">Small</Button>
-          <Button size="lg">Large</Button>
-          <Button className="bg-blue-700 p-8 font-bold">
-            Custom className
-          </Button>
-          <Button>
-            <div className="bg-yellow-200 text-stone-800">Children</div>
-          </Button>
-        </div>
+        <h2 className="text-lg font-medium mb-4">Accordion</h2>
+
+        <Accordion type="single" collapsible className="w-full">
+          <AccordionItem value="item-1">
+            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-2">
+            <AccordionTrigger>Is it styled?</AccordionTrigger>
+            <AccordionContent>
+              Yes. It comes with default styles that matches the other
+              components&apos; aesthetic.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-3">
+            <AccordionTrigger>Is it animated?</AccordionTrigger>
+            <AccordionContent>
+              Yes. It&apos;s animated by default, but you can disable it if you
+              prefer.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
       </section>
 
       <section className="mb-8">
